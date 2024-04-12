23520a9a504a84cc68ab1641843dd4f02c124e31
Matthew Davis <matthew@matthewdavis.uk>
2024-04-12 08:32:41 +0100

    Create script.ts

---
+++ src/script.ts
@@ -0,0 +1,12 @@
+const el = document.querySelector('#header');
+const button = document.querySelector('button');
+
+const partInsult = "You're nothing but a ";
+const listA = ["filthy ", "crusty ", "rusty ", "stupid ", "stinky ", "lumpy ", "rotten ", "lazy ", "smelly ", "squishy ", "no good ", "brainless "];
+const listB = ["crap ", "butt ", "turd ", "bum ", "poop ", "puss ", "rod ", "puke ", "weiner ", "fart ", "nut ", "trash ", "dong "];
+const listC = ["mammoth!", "explorer!", "lizard!", "wizard!", "tunnel!", "fairy!", "hamster!", "captain!", "dragon!", "weasel!", "goblin!", "pirate!", "banjo!"];
+
+const random = (list) => list[Math.floor(Math.random() * list.length)];
+const insult = () => partInsult + random(listA) + random(listB) + random(listC);
+
+button.addEventListener('click', () => el.innerHTML = insult());
